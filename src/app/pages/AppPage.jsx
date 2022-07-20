adaf925592cbed8be9237377fedf03550345e712
USERSAD\gerheinz <german_1906@hotmail.com>
2022-07-20 21:32:44 +0200

    Creating Note View

--- src/app/pages/AppPage.jsx
+++ src/app/pages/AppPage.jsx
@@ -2,10 +2,13 @@ import { Typography } from "@mui/material"
 import { HomeLayout } from "../../auth/layout/HomeLayout"
 import { NothingSelectedView } from "../views/NothingSelectedView"
 
+import { NoteView } from "../views/NoteView"
+
 export const AppPage = () => {
   return (
   <HomeLayout>
-    <NothingSelectedView/>
+    {/* <NothingSelectedView/> */}
+    <NoteView/>
   </HomeLayout>
   )
 }
