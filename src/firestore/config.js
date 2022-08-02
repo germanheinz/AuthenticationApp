79a7fc8ef7ed0bd75b4ced5786192d9dcb9b9dfc
USERSAD\gerheinz <german_1906@hotmail.com>
2022-08-02 20:20:49 +0200

    Firebase - Config

---
+++ src/firestore/config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirebaseStore } from 'firebase/storage';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCUg6fK0bSt40cya5UKjgJt82kopQPLdqY",
+  authDomain: "react-auth-f20f2.firebaseapp.com",
+  databaseURL: "https://react-auth-f20f2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "react-auth-f20f2",
+  storageBucket: "react-auth-f20f2.appspot.com",
+  messagingSenderId: "982703667742",
+  appId: "1:982703667742:web:a56f1087686ae724104c0f"
+};
+
+// Initialize Firebase
+const firebaseApp   = initializeApp(firebaseConfig);
+const firebaseAuth  = getAuth(firebaseApp);
+const firebaseDB = getFirebaseStore(firebaseApp);
