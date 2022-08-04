c9b3892f235cfebdc497bf592cab1c7ea5279652
USERSAD\gerheinz <german_1906@hotmail.com>
2022-08-04 11:13:51 +0200

    Form - Custom Validation

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -8,6 +8,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import KeyIcon from '@mui/icons-material/Key';
 import { useForm } from '../../hooks/useForm';
+import { useState } from 'react';
 
 
 const formDataTest = {
@@ -19,11 +20,21 @@ const formDataTest = {
 
 export const RegisterPage = () => {
 
-  const { displayName, email, password, formState, onInputChange } = useForm( formDataTest );
+  const [ formSubmitted, setFormSubmitted ] = useState(false);
+
+  const formValitations = {
+    displayName: [ (value) => value.length >= 1, 'Error: name is required' ],
+    email:[ (value) => value.includes('@'), 'Error: email is required'],
+    password:[ (value) => value.length >= 4, 'Error: password is required' ] 
+  }
+
+  const { displayName, email, password,
+          displayNameValid, emailValid, passwordValid, formState, onInputChange, formValidation, isFormValid } = useForm( formDataTest, formValitations );
 
 
   const onSubmit = ( e ) => {
     e.preventDefault();
+    setFormSubmitted(true);
     console.log( formState );
   }
 
@@ -43,19 +54,19 @@ export const RegisterPage = () => {
              <Grid item xs={ 12 } sx={{ mt: 2 }}>
                <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
                  <KeyIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                 <TextField id="input-with-sx" name="displayName" value={ displayName } onChange={ onInputChange } label="Name" type="text" placeholder='Name' variant="standard" fullWidth />
+                 <TextField id="input-with-sx" name="displayName" value={ displayName } onChange={ onInputChange } label="Name" type="text" placeholder='Name' variant="standard" error={!!displayNameValid && formSubmitted} helperText={ displayNameValid } fullWidth />
                </Box>
              </Grid>
              <Grid item xs={ 12 } sx={{ mt: 2 }}>
              <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
                  <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                 <TextField id="input-with-sx" name="email" value={ email } onChange={ onInputChange } label="Email" type="email" placeholder='Email' variant="standard" fullWidth />
+                 <TextField id="input-with-sx" name="email" value={ email } onChange={ onInputChange } label="Email" type="email" placeholder='Email' variant="standard" error={!!emailValid && formSubmitted } helperText={ emailValid } fullWidth />
                </Box>
              </Grid>
              <Grid item xs={ 12 } sx={{ mt: 2 }}>
                <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
                  <KeyIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                 <TextField id="input-with-sx" name="password" value={ password } onChange={ onInputChange } label="Password" type="password" placeholder='Password' variant="standard" fullWidth />
+                 <TextField id="input-with-sx" name="password" value={ password } onChange={ onInputChange } label="Password" type="password" placeholder='Password' error={!!passwordValid && formSubmitted } helperText={ passwordValid } variant="standard" fullWidth />
                </Box>
              </Grid>
              <Grid container spacing={ 5 } sx={{ mb: 2, mt: 1 }}>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,8 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
-export const useForm = ( initialForm = {} ) => {
+export const useForm = ( initialForm = {}, formValitations ) => {
   
     const [ formState, setFormState ] = useState( initialForm );
+    const [ formValidation, setformValidation ] = useState( initialForm );
+
+    useEffect(() => {
+    
+        createValidations();
+
+    }, [formState]);
+    
+    
+    const isFormValid = useMemo(()=> {
+
+        for (const formValue of Object.keys(formValidation) ){
+            if(formValidation[formValue] !== null) return false;
+        }
+        return true;
+    
+    }, [formValidation])
 
     const onInputChange = ({ target }) => {
         const { name, value } = target;
@@ -16,10 +33,32 @@ export const useForm = ( initialForm = {} ) => {
         setFormState( initialForm );
     }
 
+    const createValidations = () => {
+
+        const formCheckedValues = {};
+
+        for (const formField of Object.keys( formValitations )) {
+            const [ fn, errorMessage ] = formValitations[formField];
+            
+            formCheckedValues[`${formField}Valid`] = fn( formState[formField]) ? null : errorMessage;
+
+            
+        }
+        console.log(formCheckedValues);
+        
+
+        setformValidation(formCheckedValues);
+
+
+    }
+
+
     return {
         ...formState,
         formState,
         onInputChange,
         onResetForm,
+        ...formValidation,
+        isFormValid
     }
 }
